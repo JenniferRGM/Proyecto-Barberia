895b2fc86c7de9d2638d8ffcbac31ad575b8ecaa
Jennifer García <jennifer2417@gmail.com>
2025-12-01 17:24:09 -0600

    Modification de las tablas clientes y barberos
    Se agrega la modificación de las tablas clientes y barberos para agregar el consentimiento de la políticas.

---
+++ Consentimiento.sql
@@ -0,0 +1,21 @@
+--Agregar el consentimiento en las tablas Clientes y Barberos
+ALTER TABLE Clientes
+ADD
+    Consentimiento      BIT NOT NULL CONSTRAINT DF_Clientes_Consent DEFAULT 0,
+    FechaConsentimiento DATETIME NULL;
+
+ALTER TABLE Barberos
+ADD
+    Consentimiento      BIT NOT NULL CONSTRAINT DF_Barberos_Consent DEFAULT 0,
+    FechaConsentimiento DATETIME NULL;
+
+--Marcar clientes y barberos existentes como aceptado
+UPDATE Clientes
+SET Consentimiento = 1,
+    FechaConsentimiento = GETDATE()
+WHERE Consentimiento = 0;
+
+UPDATE Clientes
+SET Consentimiento = 1,
+    FechaConsentimiento = GETDATE()
+WHERE Consentimiento = 0;
